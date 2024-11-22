24d71cc5cb9cf7ec7dd02a2fdd792527b3c05c38
zolianda <zoliandaam@gmail.com>
2024-11-22 15:37:48 +0700

    keknya punya YANDA

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import RegisterPage from './Pages/RegisterPage.jsx'
 import ErrorPage from './Pages/404.jsx'
 import ContohState from './ContohState.jsx'
 import Product from './Pages/Product.jsx'
-import LoginPage from './Pages/loginPage.jsx'
+import LoginPage from './Pages/LoginPage.jsx'
 
 
 const router = createBrowserRouter([
