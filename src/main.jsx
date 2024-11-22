275add63b58cf1b89787a50879bd6cb4814a1faa
zolianda <zoliandaam@gmail.com>
2024-11-22 15:33:13 +0700

    keknya punya YANDA

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import './index.css'
-import LoginPage from './Pages/loginPage.jsx'
 import RegisterPage from './Pages/RegisterPage.jsx'
 import ErrorPage from './Pages/404.jsx'
 import ContohState from './ContohState.jsx'
 import Product from './Pages/Product.jsx'
+import LoginPage from './Pages/loginPage.jsx'
 
 
 const router = createBrowserRouter([
